c353467fbc8b30decf7f2ad54333cd736b68e12f
laluxx <lorenzoaddamo10@gmail.com>
2022-11-06 22:18:47 +0100

    Add files via upload

--- pkg-files/xos.sh
+++ pkg-files/xos.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
-mkdir ~/.config/suckless
+#mkdir ~/.config/suckless
 
+mkdir  /home/$USERNAME/.config/suckless
+cd /home/$USERNAME/.config/suckless 
 
 echo -nE "
  
@@ -8,13 +10,13 @@ echo -nE "
  █░░█ █▄█▄█ █░▀░█ 
  ▀▀▀░ ░▀░▀░ ▀░░░▀
 "
-
 wget https://dl.suckless.org/dwm/dwm-6.4.tar.gz
 tar -xzvf dwm-6.4.tar.gz
+rm dwm-6.4.tar.gz
 cd dwm-6.4/
 sudo make clean install
 cd ..
-mv dwm-6.4/ ~/.config/suckless
+#mv dwm-6.4/ ~/.config/suckless
 
 
 echo -nE "
@@ -25,10 +27,11 @@ echo -nE "
 "
 wget https://dl.suckless.org/tools/dmenu-5.2.tar.gz
 tar -xzvf dmenu-5.2.tar.gz
+rm dmenu-5.2.tar.gz
 cd dmenu-5.2/
 sudo make clean install
 cd ..
-mv dmenu-5.2/ ~/.config/suckless
+#mv dmenu-5.2/ ~/.config/suckless
 
 
 echo -nE "
@@ -39,10 +42,11 @@ echo -nE "
 "
 wget https://dl.suckless.org/st/st-0.9.tar.gz 
 tar -xzvf st-0.9.tar.gz
+rm st-0.9.tar.gz
 cd st-0.9/
 sudo make clan install
 cd ..
-mv st-0.9/ ~/.config/suckless
+# mv st-0.9/ ~/.config/suckless
 
 
 
